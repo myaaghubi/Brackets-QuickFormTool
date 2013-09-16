46b89334fd1394cbd8049216b0334eb81e87e14c
Mohammad Yaghobi <m.yaghobi.abc@gmail.com>
2013-09-16 17:39:58 +0430

    Make main.js file

---
+++ main.js
@@ -0,0 +1,8 @@
+//ProFormTool ver 0.1
+//An extension for https://github.com/adobe/brackets
+//This extension currently not available for use so is incompletely.
+
+define(function (require, exports, module)
+{
+    "use strict";
+});
