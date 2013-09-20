93d03967dec607396f5252e3ab226ee3c3b8d80e
Mohammad Yaghobi <m.yaghobi.abc@gmail.com>
2013-09-20 20:43:37 +0330

    Update main.js

--- main.js
+++ main.js
@@ -7,19 +7,26 @@ define(function (require, exports, module)
 {
     "use strict";
     
-    var EditorManager = brackets.getModule("editor/EditorManager"),
-        ExtensionUtils = brackets.getModule("utils/ExtensionUtils"),
-        InlineWidget = brackets.getModule("editor/InlineWidget").InlineWidget,
-        Strings = brackets.getModule("strings"),
+    var EditorManager         = brackets.getModule("editor/EditorManager"),
+        ExtensionUtils        = brackets.getModule("utils/ExtensionUtils"),
+        InlineWidget          = brackets.getModule("editor/InlineWidget").InlineWidget,
+        Strings               = brackets.getModule("strings"),
         QuickFormToolTemplate = require("text!ui/QuickFormToolTemplate.html");
 
-    function QuickFormTool()
+    function QuickFormTool(position)
     {
+		this._position = position;
         InlineWidget.call(this);
+		function onAdded()
+		{
+			this.hostEditor.setinlineWidgetHeight(this, this.FormContent.getRootElement().outerHeight(), true);
+			this.FormContent.focus();
+		};
     }
 
     QuickFormTool.prototype = Object.create(InlineWidget.prototype);
     QuickFormTool.prototype.parentClass = InlineWidget.prototype;
+	QuickFormTool.prototype._position = null;
 
     QuickFormTool.prototype.load = function ()
     {
@@ -28,21 +35,14 @@ define(function (require, exports, module)
         this.$htmlContent.append(this.$element);
     };
 
-    QuickFormTool.prototype.onAdded = function ()
-    {
-        this.hostEditor.setinlineWidgetHeight(this, this.FormContent.getRootElement().outerHeight(), true);
-        this.FormContent.focus();
-    };
 
-    //----------------------------
-	
     function quickFormToolProvider(hostEditor, pos)
     {
-        var quickformtoolline, result, start, end;
-        pos.ch = start;
+        var quickformtoolline, result, position, end;
+        position = pos;
         hostEditor.setSelection(pos, { line: pos.line, ch: end });
 
-        quickformtoolline = new QuickFormTool();
+        quickformtoolline = new QuickFormTool(position);
         quickformtoolline.load(hostEditor);
         result = new $.Deferred();
         result.resolve(quickformtoolline);
