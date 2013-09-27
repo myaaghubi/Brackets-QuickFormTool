286b195ea4003dec0267635f513a34c555d469d6
Mohammad Yaghobi <m.yaghobi.abc@gmail.com>
2013-09-27 19:54:20 +0330

    firs utilizable version

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-//QuickFormTool ver 0.5
+//QuickFormTool ver 0.6 beta
 //An extension for https://github.com/adobe/brackets
+//Last version of extension https://github.com/pgrammer/Brackets-QuickFormTool
 //This extension currently not available for use so is incompletely.
-//by M.Yaghobi, m.yaghobi.abc@gmail.com
+//by Mohammad.Yaghobi, m.yaghobi.abc@gmail.com
 
 define(function (require, exports, module)
 {
@@ -9,47 +10,94 @@ define(function (require, exports, module)
     
     var EditorManager         = brackets.getModule("editor/EditorManager"),
         ExtensionUtils        = brackets.getModule("utils/ExtensionUtils"),
-        InlineWidget          = brackets.getModule("editor/InlineWidget").InlineWidget,
         Strings               = brackets.getModule("strings"),
         QuickFormToolTemplate = require("text!ui/QuickFormToolTemplate.html");
 
-    function QuickFormTool(position)
+	
+    function quickFormToolProvider()
     {
-		this._position = position;
-        InlineWidget.call(this);
-		function onAdded()
-		{
-			this.hostEditor.setinlineWidgetHeight(this, this.FormContent.getRootElement().outerHeight(), true);
-			this.FormContent.focus();
-		};
+        try
+        {
+            var $element = $(Mustache.render(QuickFormToolTemplate, Strings));
+            $element.find(".form").click(function () { quickFormTool("form"); });
+			$element.find(".textfield").click(function () { quickFormTool("textfield"); });
+			$element.find(".textarea").click(function () { quickFormTool("textarea"); });
+			$element.find(".botton").click(function () { quickFormTool("botton"); });
+			$element.find(".checkbox").click(function () { quickFormTool("checkbox"); });
+			$element.find(".radiobotton").click(function () { quickFormTool("radiobotton"); });
+			$element.find(".listmenu").click(function () { quickFormTool("listmenu"); });
+			$element.find(".imagefield").click(function () { quickFormTool("imagefield"); });
+			$element.find(".filefield").click(function () { quickFormTool("filefield"); });
+			$element.find(".hiddenfiled").click(function () { quickFormTool("hiddenfiled"); });
+
+            $($element).insertBefore("#editor-holder");
+        }
+        catch(e)
+        {
+            alert("Error: " + e);
+        }
     }
-
-    QuickFormTool.prototype = Object.create(InlineWidget.prototype);
-    QuickFormTool.prototype.parentClass = InlineWidget.prototype;
-	QuickFormTool.prototype._position = null;
-
-    QuickFormTool.prototype.load = function ()
-    {
-        QuickFormTool.prototype.parentClass.load.apply(this, arguments);
-        this.$element = $(Mustache.render(QuickFormToolTemplate, Strings));
-        this.$htmlContent.append(this.$element);
-    };
-
-
-    function quickFormToolProvider(hostEditor, pos)
-    {
-        var quickformtoolline, result, position, end;
-        position = pos;
-        hostEditor.setSelection(pos, { line: pos.line, ch: end });
-
-        quickformtoolline = new QuickFormTool(position);
-        quickformtoolline.load(hostEditor);
-        result = new $.Deferred();
-        result.resolve(quickformtoolline);
-        return result.promise();
-    }
-    
+    var loadfunction = quickFormToolProvider();
+	function quickFormTool(_class)
+	{
+        try
+        {
+			EditorManager.focusEditor();
+			var hosteditor = EditorManager.getFocusedEditor();
+			if (hosteditor) 
+			{
+				var htmlcode = "";
+				switch (_class)
+				{
+					case "form":
+						htmlcode = "<form action='' method='get'></form>";
+					break;
+					
+					case "textfield":
+						htmlcode = "<input name='' type='text' />";
+					break;
+					
+					case "textarea":
+						htmlcode = "<textarea name='' cols='' rows=''></textarea>";
+					break;
+					
+					case "botton":
+						htmlcode = "<input name='' type='button' />";
+					break;
+					
+					case "checkbox":
+						htmlcode = "<input name='' type='checkbox' value='' />";
+					break;
+					
+					case "radiobotton":
+						htmlcode = "<input name='' type='radio' value='' />";
+					break;
+					
+					case "listmenu":
+						htmlcode = "<select name=''></select>";
+					break;
+					
+					case "imagefield":
+						htmlcode = "<input name='' type='image' src='asd' align='middle' width='' height='' />";
+					break;
+					
+					case "filefield":
+						htmlcode = "<input name='' type='file' />";
+					break;
+					
+					case "hiddenfiled":
+						htmlcode = "<input name='' type='hidden' value='' />";
+					break;
+				}
+				hosteditor.document.replaceRange(htmlcode, hosteditor.getCursorPos());
+			}
+        }
+        catch(e)
+        {
+            alert("Error: " + e);
+        }
+	};
+	
     ExtensionUtils.loadStyleSheet(module, "ui/main.css");
-    EditorManager.registerInlineEditProvider(quickFormToolProvider);
     exports.quickFormToolProvider = quickFormToolProvider;
 });
